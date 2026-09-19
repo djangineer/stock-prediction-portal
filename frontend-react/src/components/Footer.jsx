304d9a1f1478b9908cd4d43a8cf560239eb2a685
devXpress <ekpenyonabasi@gmail.com>
2026-09-19 22:03:51 +0100

    homepage

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="footer py-3 my-3">
+        <hr className='border-bottom'/>
+        <p className="text-center text-light">&copy; 2026 Stock Prediction Portal. Tally Technologies Limited. All rights reserved.</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
